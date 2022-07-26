b86ede108182cfe466624245f70bcbf8c49a7403
Thulio Paiva <thuliopv360@gmail.com>
2022-07-25 21:53:45 -0300

    feature: creation the cards pokemons

--- concepts/src/pages/Home/index.jsx
+++ concepts/src/pages/Home/index.jsx
@@ -1,17 +1,26 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import Card from "../../components/Card"
 
 const Home = () => {
     const [pokemons, setPokemons ] = useState([]);
 
-    const fetchPokemon = async () =>{
+    const fetchPokemons = async () =>{
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
         console.log(response);
-    }
+
+        const pokemonsList = await response.json()
+        setPokemons(pokemonsList.results);
+    };
+
+    useEffect(() => {
+        fetchPokemons()
+    }, [])
 
     return (
         <div>
             <h1>Home</h1>
-
+            {pokemons.map((element,index) => <Card pokemon={element} key={index}/>)}
         </div>
     );
 };
